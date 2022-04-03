a08b22b12de4a090be0e7de4a8947a768449e93b
yousef <yousef_salih@hotmail.com>
2022-04-03 16:07:09 +0300

    Including JavaScript file and exercises

---
+++ script.js
@@ -0,0 +1,28 @@
+//Practicing working with different data types and values
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+console.log(49 + 9 + 9 - 15);
+
+let country = "Turkey";
+let continent = "Asia";
+let population = 84340000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(typeof country);
+
+//Practice working with different operators
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
