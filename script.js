55b454868043509cfb24df0945e4302fff6d63c8
yousef <yousef_salih@hotmail.com>
2022-04-05 01:58:47 +0300

    Coding Challenge #6 working with arrays and expanding on the calculator

--- script.js
+++ script.js
@@ -76,20 +76,6 @@ if(averageDolphinScore > averageKoalaScore && averageDolphinScore >= minimumScor
     else {console.log('No one wins!')}
 
     // Coding Challenge #4
-    // Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
-    // Your tasks:
-    // 1. Calculate the tip,depending on the bill value.Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
-    // 2. Print a string to the console containing the bill value,the tip,and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
-    // Test data:
-    // § Data 1: Test for bill values 275, 40 and 430 
-
-    let billValue = 430
-    let tip = billValue >= 50 && billValue <= 300 ? 0.15 * billValue : 0.2 * billValue
-    let finalValue = billValue + tip
-
-    console.log(`The bill was ${billValue}, the tip is ${tip} and the final value ${finalValue}`)
-
-    // Coding Challenge #5
     // Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
     // Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
     // A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
@@ -129,3 +115,37 @@ if(averageDolphinScore > averageKoalaScore && averageDolphinScore >= minimumScor
 
     checkWinner(avgDolphins,avgKoalas);
 
+        // Coding Challenge #5
+    // Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+    // Your tasks:
+    // 1. Calculate the tip,depending on the bill value.Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+    // 2. Print a string to the console containing the bill value,the tip,and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+    // Test data:
+    // § Data 1: Test for bill values 275, 40 and 430 
+
+    let billValue = 100
+    let tip = billValue >= 50 && billValue <= 300 ? 0.15 * billValue : 0.2 * billValue
+    let finalValue = billValue + tip
+
+    console.log(`The bill was ${billValue}, the tip is ${tip} and the final value ${finalValue}`)
+
+    // Coding Challenge #6
+    // Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+    // Your tasks:
+    // 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+    // 2. And now let's use arrays!So create an array 'bills' containing the test data below
+    // 3.Create an array 'tips' containing the tip value for each bill,calculated from the function you created before
+    // 4.Bonus:Create an array 'total' containing the total values,so the bill+tip 
+    // Test data: 125, 555 and 44
+    // Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+    const calcTip = function (bills) {
+        return bills >= 50 && bills <= 300 ? 0.15 * bills : 0.2 * bills
+    }
+    const bills = [125, 555, 44];
+    const tips = [calcTip(bills[0]), calcTip(bills[1]) ,calcTip(bills[2])];
+    console.log(bills, tips);
+    const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2] ]
+    console.log(total)
+
+    
