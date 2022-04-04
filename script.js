f9c09145257363e2d833b307ada76b0016ef94e2
yousef <yousef_salih@hotmail.com>
2022-04-04 15:59:38 +0300

    Coding challenge 3 logical operators

--- script.js
+++ script.js
@@ -51,3 +51,27 @@ if(markBMI > johnBMI){
     console.log(`John's BMI is higher than Mark's!`)
 }
 
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team,using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus1:Include a requirement for a minimum score of 100.With this rule,a
+// team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+// 4. Bonus2:Minimum score also applies to a draw!So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+
+let averageDolphinScore = (96+108+89)/3
+let averageKoalaScore = (88+91+110)/3
+let minimumScore = 100
+
+if(averageDolphinScore > averageKoalaScore && averageDolphinScore >= minimumScore) {
+    console.log('Dolphins Win!')
+} else if (averageDolphinScore === averageKoalaScore) {
+    console.log('Its a tie!')
+} else if (averageKoalaScore >averageDolphinScore && averageKoalaScore>= minimumScore) 
+    {console.log('Koalas Win!')} 
+    else {console.log('No one wins!')}
+
