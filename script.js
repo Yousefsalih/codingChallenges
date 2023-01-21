912e85e7d31321681d4878ffe432acf5fbf50f88
yousef <yousef_salih@hotmail.com>
2023-01-21 15:37:58 +0300

    Coding challange #8

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ if(averageDolphinScore > averageKoalaScore && averageDolphinScore >= minimumScor
 // Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 // Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
 // tips and totals arrays 😉 Bonus:
-// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+// 4. Bonus: Write a function 'calcAverage1' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
 // 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
 // 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 // 4.3. Call the function with the 'totals' array
@@ -203,6 +203,25 @@ const billz = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tipz = [];
 const totalz = [];
 
-for (let billz = 0; billz <= billz.length; billz++ ) {
-    console.log(billz)
-}
+for (let i = 0; i <= billz.length; i++ ) {
+    let tipValue = calcTip(billz[i]);
+    tipz.push(tipValue);
+    let total = tipValue + billz[i];
+    totalz.push(total);
+};
+
+console.log(tipz);
+console.log(totalz);
+
+const calcAverage2 = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]; //same as sum = sum + arr[i];
+    }
+    // console.log(sum)
+    return sum / arr.length;
+};
+
+console.log(calcAverage2([2, 3, 6]));
+console.log(calcAverage2(totalz));
+console.log(calcAverage2(tipz));
