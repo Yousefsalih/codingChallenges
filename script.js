cf0b88801ac7644dc33f5d062a0fc7e1d2b1bd34
yousef <yousef_salih@hotmail.com>
2023-02-04 14:08:05 +0300

    Coding challange #15

--- script.js
+++ script.js
@@ -628,3 +628,19 @@ document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer
 //Bonus: Using the call method, it replaces the 'this' keyword in the function displayResults with the array below in the new object
 poll.displayResults.call({answers: [5, 2, 3]}, 'string'); //Array(4) [ 1, 0, 0, 0 ] Poll results are 5, 2, 3
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1] }); //Array(6) [ 1, 5, 3, 9, 6, 1 ]
+
+// Coding Challenge #15
+// This is more of a thinking challenge than a coding challenge 🤓 Your tasks:
+// 1. Take the IIFE below and at the end of the function,attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or some one around you) why this worked!Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example.
+
+(function () {
+const header = document.querySelector('h1'); header.style.color = 'red';
+
+document.querySelector('body').addEventListener('click', function() {
+  header.style.color = 'blue'
+});
+})();
+
+//The header is changed to red the first time the function is executed (IIFE), which creates the variables. Now the function is finished executing and waiting for the click to happen. Because of the click, a new function is being called which changes the header to blue. This is due to closure.
+
